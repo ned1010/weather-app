ad32630872a82fe4706d509bec161e4ec0686a18
nidup1010 <nidup1998@gmail.com>
2023-07-07 14:13:25 +0600

    changed file path in ThemeProvider.jsx

--- src/utilities/ThemeProvider.jsx
+++ src/utilities/ThemeProvider.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import ThemeContext from "./themeContext";
+import ThemeContext from "./ThemeContext";
 
 const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("dark");
