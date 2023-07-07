9b4f9414017663f23cc9ddbf079ed4188496425d
nidup1010 <nidup1998@gmail.com>
2023-07-07 14:08:16 +0600

    changed path to the ToggleButton.jsx

--- src/utilities/ToggleButton.jsx
+++ src/utilities/ToggleButton.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import ThemeContext from "./themeContext";
+import ThemeContext from "./ThemeContext";
 import { IconButton } from "@mui/material";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
